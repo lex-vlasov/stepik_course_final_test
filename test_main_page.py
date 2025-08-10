07fa2049507f9cca17db686b0181f5415cf84d1f
Aleksei Vlasov <lexvlasov@gmail.com>
2025-08-10 17:20:03 +0200

    Add tests and locators for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 # def go_to_login_page(browser):
@@ -19,3 +20,25 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+#Login page tests
+
+def test_login_page_url_is_correct(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_login_form_is_shown_on_login_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_register_form_is_shown_on_login_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
