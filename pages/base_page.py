6f7eaad23c1a4021cb04ae6843544eae3b32356e
Aleksei Vlasov <lexvlasov@gmail.com>
2025-08-15 23:23:20 +0200

    refactor tests, locators, methods, move to base page common tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,8 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from .locators import BasePageLocators
+
 
 class BasePage():
     def __init__(self, browser, url):
@@ -56,3 +58,11 @@ class BasePage():
             return False
 
         return True
+
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+     pass
 
 
 class LoginPageLocators():
@@ -15,3 +15,9 @@ class ProductPageLocators():
     PRODUCT_IN_ADD_BASKET_MESSAGE = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
     TOTAL_BASKET_PRICE = (By.XPATH, "//*[@id='messages']/div[3]/div//strong")
     ADD_TO_BASKET_BUTTON = (By.XPATH, "//button[contains(@class, 'btn-add-to-basket') and (@type='submit')]")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
